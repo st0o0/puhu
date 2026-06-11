aa557ee761d0ed418e05c530a61d268e4ebe5f26
st0o0 <64534642+st0o0@users.noreply.github.com>
2026-06-11 23:33:58 +0200

    feat: add setup chain, tab bar, plugin routing, and Program.cs entry point

--- src/Servus.Runner/Program.cs
+++ src/Servus.Runner/Program.cs
@@ -1,1 +1,14 @@
-Console.WriteLine("Servus!");
+using Microsoft.Extensions.Hosting;
+using Servus.Application.Startup;
+using Servus.Runner.Setup;
+
+var runner = AppBuilder.Create(Host.CreateApplicationBuilder(args), b => b.Build())
+    .WithSetup<LoggingSetup>()
+    .WithSetup<ServicesSetup>()
+    .WithSetup<MarketplaceSetup>()
+    .WithSetup<PluginSetup>()
+    .WithSetup<ActorSystemSetup>()
+    .WithSetup<TerminaSetup>()
+    .Build();
+
+await runner.RunAsync();
---
+++ src/Servus.Runner/Setup/MarketplaceSetup.cs
@@ -0,0 +1,29 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Servus.Application.Startup;
+using Servus.Plugin.Marketplace.Models;
+using Servus.Plugin.Marketplace.Services;
+
+namespace Servus.Runner.Setup;
+
+public sealed class MarketplaceSetup : IServiceSetupContainer
+{
+    public void SetupServices(IServiceCollection services, IConfiguration configuration)
+    {
+        var basePath = Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.UserProfile),
+            ".servus");
+        var pluginsDir = Path.Combine(basePath, "plugins");
+
+        var configStore = new PluginConfigStore(basePath);
+        var cache = new PluginCache(Path.Combine(basePath, "cache"));
+        var httpClient = new HttpClient();
+        httpClient.DefaultRequestHeaders.Add("User-Agent", "servus-plugin-manager");
+        var fetcher = new PluginMetadataFetcher(httpClient, cache);
+        var downloader = new PluginDownloader(httpClient, pluginsDir);
+        var manager = new PluginManager(configStore, fetcher, downloader, pluginsDir);
+
+        services.AddSingleton(configStore);
+        services.AddSingleton<IPluginManager>(manager);
+    }
+}
