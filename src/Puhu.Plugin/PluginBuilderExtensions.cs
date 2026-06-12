86efa803a169bd1e816cc33513d60cc479348fee
st0o0 <64534642+st0o0@users.noreply.github.com>
2026-06-12 07:12:28 +0200

    feat: add WithSettings<TPage, TViewModel> extension method

---
+++ src/Puhu.Plugin/PluginBuilderExtensions.cs
@@ -0,0 +1,21 @@
+using Termina.Hosting;
+using Termina.Reactive;
+
+namespace Puhu.Plugin;
+
+public static class PluginBuilderExtensions
+{
+    public static IPuhuPluginBuilder WithSettings<TPage, TViewModel>(
+        this IPuhuPluginBuilder builder,
+        string label)
+        where TPage : ReactivePage<TViewModel>
+        where TViewModel : ReactiveViewModel
+    {
+        var route = $"/settings/{label.ToLowerInvariant().Replace(' ', '-')}";
+
+        builder.WithSettings(label, route);
+        builder.WithRoutes(termina => termina.RegisterRoute<TPage, TViewModel>(route));
+
+        return builder;
+    }
+}
